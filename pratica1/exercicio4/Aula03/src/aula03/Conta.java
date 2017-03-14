49a7de6bccd79401820f3579bcfca22c5723706c
André Chioratto <Andr Chioratto>
2017-03-14 00:01:24 -0300

    exercicio4

---
+++ pratica1/exercicio4/Aula03/src/aula03/Conta.java
@@ -0,0 +1,24 @@
+
+package aula03;
+
+
+public class Conta {
+    int numero;
+    String cpf;
+    float saldo;
+    double titular;
+      
+void visualizarSaldo (){
+    System.out.println("Saldo = " + this.saldo);
+}
+void depositar(){
+    
+}
+void sacar (){
+    
+}
+void transferirDinheiro(){
+    
+}
+
+}
