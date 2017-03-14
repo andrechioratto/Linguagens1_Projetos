707d798babbfee76a0b6d39b3ed286b6f01aacc8
Aluno <Aluno>
2017-03-14 11:07:55 -0300

    exercicio4

---
+++ pratica1/exercicio4/Aula03/src/aula03/Cliente.java
@@ -0,0 +1,17 @@
+
+package aula03;
+
+
+public class Cliente {
+  
+       String nome;
+       String sobrenome;
+       String cpf;
+       
+    
+    void visualizarConta (){
+        System.out.println("Cliente = " + this.nome + this.sobrenome);
+        System.out.println("Cpf =" this.cpf);
+}
+}
+
