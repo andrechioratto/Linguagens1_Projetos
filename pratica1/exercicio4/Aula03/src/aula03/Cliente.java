6309b63a47358eb82d34e2bdbfa1f1aaaf386223
andrechioratto <andrechioratto_@hotmail.com>
2017-03-15 10:24:34 -0300

    Update Cliente.java

--- pratica1/exercicio4/Aula03/src/aula03/Cliente.java
+++ pratica1/exercicio4/Aula03/src/aula03/Cliente.java
@@ -1,17 +1,17 @@
 
+
 package aula03;
 
 
 public class Cliente {
   
-       String nome;
-       String sobrenome;
-       String cpf;
+      String nome;
+      String sobrenome;
+      String cpf;
        
     
     void visualizarConta (){
         System.out.println("Cliente = " + this.nome + this.sobrenome);
-        System.out.println("Cpf =" this.cpf);
+        System.out.println("Cpf = " + this.cpf);
 }
 }
-
